2ee4ea985d2cee65ca17058238017306c7e7c09f
KerwinJhong <sh940718@gmail.com>
2020-03-03 17:12:57 +0800

    fix sitemap link in head

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Serif+TC&display=swap' },
-      { rel="sitemap", type="application/xml", title="Sitemap", href="https://www.kerwinresume.gq/sitemap.xml" }
+      { rel: 'sitemap', type: 'application/xml', title: 'Sitemap', href: 'https://www.kerwinresume.gq/sitemap.xml' }
     ]
   },
   /*
